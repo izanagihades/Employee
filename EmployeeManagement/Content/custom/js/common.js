e8b482c1f14daab1340734cb8faab697bedf7325
NGOCMLSE62083\NgocMLSE62083 <ngocmalam2509@gmail.com>
2017-10-18 19:57:55 +0700

    a

---
+++ EmployeeManagement/Content/custom/js/common.js
@@ -0,0 +1,21 @@
+﻿/*Nguyen Hung Manh*/
+"use strict";
+function getQueryString() {
+    var searchUrl = window.location.search;
+    var urlParams = searchUrl.substr(1).split('&');
+    var result = {};
+    for (var i = 0; i < urlParams.length; i++) {
+        var pair = urlParams[i].split('=');
+        result[pair[0]] = pair[1] || '';
+    }
+    return result;
+}
+
+function preventDefaultAction(event) {
+    event = event || window.event;
+    if (event.preventDefault) {
+        event.preventDefault();
+    } else {
+        event.returnValue = false;
+    }
+}
